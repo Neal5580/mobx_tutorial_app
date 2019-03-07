5ae4bbc88422f0785e2c0a5cff8f0a224685d205
Neal Shen <shenyijie5580@gmail.com>
2019-03-08 10:39:06 +1100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { observable, configure, action } from "mobx";
+import { observer } from "mobx-react";
+import DevTools from "mobx-react-devtools";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//change to strict mode
+configure({ enforceActions: "observed" });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const appState = observable({
+    count: 0,
+    incCount: action("Increment Counter", () => {
+        appState.count += 1;
+    }),
+    decCount: action("Decrement Counter", () => {
+        appState.count -= 1;
+    }),
+    get countByThree() {
+        return this.count * 3;
+    },
+    get countByFour() {
+        return this.count * 4;
+    }
+});
+
+const Counter = observer(props => (
+    <section>
+        {props.appState.count}
+        <div>
+            <button onClick={props.appState.incCount}>Add</button>
+            <button onClick={props.appState.decCount}>Dec</button>
+            <DevTools />
+        </div>
+        <div>Count by Three {props.appState.countByThree}</div>{" "}
+        <div>Count by Four {props.appState.countByFour}</div>
+    </section>
+));
+
+ReactDOM.render(
+    <Counter appState={appState} />,
+    document.getElementById("root")
+);
